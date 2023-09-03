d21158ead7fbfe3f1e696da9832654315bea245c
tntredstone <cgsteck@outlook.com>
2023-09-03 08:35:14 -0400

    Init

---
+++ src/App.svelte
@@ -0,0 +1,149 @@
+<script>
+	import Card from "./Card.svelte";
+	let front;
+	let back;
+	let group;
+
+	let cardsList = [
+		{
+			groupName: "add a new card",
+			front: "this is the front of a card",
+			back: "this is the back of a card",
+			completed: false,
+		},
+	];
+	let currentGroup = "add a new card";
+
+	let cardContainer = document.querySelector("#card-container");
+	let filteredcardsList;
+	let card;
+	function addOneCard() {
+		cardContainer = document.querySelector("#card-container");
+		filteredcardsList = cardsList.filter(
+			(card) => card.completed === false && card.groupName == currentGroup
+		);
+		card = filteredcardsList[0];
+		const cardCompoent = new Card({
+			target: cardContainer,
+			props: {
+				front: card.front,
+				back: card.back,
+				completed: card.completed,
+			},
+		});
+		cardCompoent.$on("done", () => {
+			for (let i = 0; i < cardsList.length - 1; i++) {
+				if (
+					cardsList[i].groupName === card.groupName &&
+					cardsList[i].front === card.front &&
+					cardsList[i].back === card.back
+				) {
+					cardsList[i].completed = true;
+				}
+			}
+			addOneCardCopy();
+			cardCompoent.$destroy();
+		});
+	}
+
+	function addOneCardCopy() {
+		cardContainer = document.querySelector("#card-container");
+		filteredcardsList = cardsList.filter(
+			(card) => card.completed === false
+		);
+		card = filteredcardsList[0];
+		const cardCompoent = new Card({
+			target: cardContainer,
+			props: {
+				front: card.front,
+				back: card.back,
+				completed: card.completed,
+			},
+		});
+		cardCompoent.$on("done", () => {
+			for (let i = 0; i < cardsList.length - 1; i++) {
+				if (
+					cardsList[i].groupName === card.groupName &&
+					cardsList[i].front === card.front &&
+					cardsList[i].back === card.back
+				) {
+					cardsList[i].completed = true;
+				}
+			}
+			cardCompoent.$destroy();
+		});
+	}
+	let innerGroupSelection;
+	function addCardForm() {
+		cardsList.push({
+			groupName: group,
+			front: front,
+			back: back,
+			completed: false,
+		});
+		const uniqueGroups = new Set();
+		cardsList.forEach((card) => {
+			uniqueGroups.add(card.groupName);
+		});
+		groups = Array.from(uniqueGroups);
+		innerGroupSelection = ""
+		groups.forEach((group) => {
+			innerGroupSelection += `<option value="${group}">${group}</option>`;
+			console.log(innerGroupSelection);
+		});
+		document.querySelector("#groupSelection").innerHTML = innerGroupSelection
+	}
+	let groups = [];
+
+	window.addEventListener("DOMContentLoaded", () => {
+		addOneCard();
+		const uniqueGroups = new Set();
+		cardsList.forEach((card) => {
+			uniqueGroups.add(card.groupName);
+		});
+		groups = Array.from(uniqueGroups);
+	});
+</script>
+
+<main>
+	<div id="card-container" />
+	<div id="form">
+		<div>
+			<select bind:value={currentGroup} id="groupSelection">
+				{#each groups as group}
+					<option value={group}>{group}</option>
+				{/each}
+			</select>
+			<div>
+				<input
+					type="text"
+					bind:value={front}
+					placeholder="front of card"
+				/>
+				<input
+					type="text"
+					bind:value={back}
+					placeholder="back of card"
+				/>
+				<input
+					type="text"
+					bind:value={group}
+					placeholder="group of card"
+				/>
+				<button on:click={addCardForm}>Done</button>
+			</div>
+		</div>
+	</div>
+</main>
+
+<style>
+	#form {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+
+	button {
+		user-select: none;
+	}
+</style>
---
+++ src/Card.svelte
@@ -0,0 +1,101 @@
+<script>
+    import { createEventDispatcher } from "svelte";
+    let dispatch = createEventDispatcher();
+    export let front;
+    export let back;
+    export let completed;
+    completed = !completed;
+    let visibility;
+    if (completed === false) {
+        visibility = "none";
+    } else {
+        visibility = "block";
+    }
+
+    function completedVisiableCard() {
+        document.querySelector(".visablecard").style.visibility = "visible";
+    }
+    function completedinvisibleCard() {
+        document.querySelector(".visablecard").style.visibility = "hidden";
+    }
+
+    function handleDoneClick() {
+        dispatch("done");
+    }
+    document.querySelector("main>div").style.display = visibility;
+</script>
+
+<main>
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;1,900&display=swap"
+        rel="stylesheet"
+    />
+    <div id="cards">
+        <!-- svelte-ignore a11y-click-events-have-key-events -->
+        <div class="invisablecard card" on:click={completedVisiableCard}>
+            <span>
+                <h2 id="invisCardText">{back}</h2>
+                <button on:click={handleDoneClick}>Done</button>
+            </span>
+        </div>
+        <!-- svelte-ignore a11y-click-events-have-key-events -->
+        <div class="visablecard card" on:click={completedinvisibleCard}>
+            <span>
+                <h2 id="visCardText">{front}</h2>
+            </span>
+        </div>
+    </div>
+</main>
+
+<style>
+    main {
+        display: relative;
+    }
+
+    .card {
+        position: absolute;
+        width: 350px;
+        height: 175px;
+        left: 0;
+        right: 0;
+        top: 0;
+        bottom: 0;
+        margin-left: auto;
+        margin-right: auto;
+        margin-top: auto;
+        margin-bottom: auto;
+        border-radius: 5px;
+    }
+
+    .visablecard {
+        background-color: #ef2bb8;
+        filter: drop-shadow(2px 2px 0.5px #003a61);
+    }
+
+    .invisablecard {
+        background-color: #003a61;
+        filter: drop-shadow(2px 2px 0.5px #ef2bb8);
+        color: white;
+    }
+
+    h2 {
+        user-select: none;
+        -moz-user-select: none;
+        -khtml-user-select: none;
+        -webkit-user-select: none;
+        -o-user-select: none;
+        font-family: "poppins";
+    }
+    div {
+        display: table;
+        height: 100px;
+        width: 100%;
+        text-align: center;
+    }
+    span {
+        display: table-cell;
+        vertical-align: middle;
+    }
+</style>
